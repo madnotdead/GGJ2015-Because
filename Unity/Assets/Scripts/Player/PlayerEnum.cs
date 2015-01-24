27da97fbb9bfd8c6ecf2bc33c5797c4bdababde8
Ezequiel Calderara <ezechico@gmail.com>
2015-01-24 13:43:27 -0300

    Boceto del StateManager

---
+++ Unity/Assets/Scripts/Player/PlayerEnum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum PlayerState
+{
+    Idle,
+    Active,
+    Working,
+    Returning,
+    Unconscious
+}
+
+public enum PlayerTaskType
+{
+    Singing,
+    FixingConsole,
+    HoldingWall,
+    CatchingCeiling,
+    CatchingLight,
+    ExtinguishingFire
+}
+
+public enum PlayerMoodTypes
+{
+    Normal,
+    Worried,
+    Afraid,
+    Happy,
+}
---
+++ Unity/Assets/Scripts/Player/PlayerStateManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStateManager : MonoBehaviour
+{
+    public PlayerState CurrentState = PlayerState.Idle;
+    public PlayerTaskType CurrentTask = PlayerTaskType.Singing;
+    public PlayerMoodTypes CurrentMood = PlayerMoodTypes.Normal;
+
+    private Vector3 InitialPosition;
+
+    public Animation[] animations;
+    void Awake()
+    {
+        InitialPosition = transform.position;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (CurrentState)
+        {
+        }
+	}
+
+    void SetActive()
+    {
+        switch (CurrentState)
+        {
+            case PlayerState.Idle:
+                EnableMovement();
+                break;
+            case PlayerState.Active:
+                EnableMovement();
+                break;
+            case PlayerState.Working:
+                EnableMovement();
+                break;
+            case PlayerState.Returning:
+                EnableMovement();
+                CurrentState = PlayerState.Active;
+                break;
+            case PlayerState.Unconscious:
+                break;
+            default:
+                break;
+        }
+    }
+    void SetInactive()
+    {
+        DisableMovement();
+        switch (CurrentState)
+        {
+            case PlayerState.Idle:
+                break;
+            case PlayerState.Active:
+                ReturnToInitialPosition();
+                break;
+            case PlayerState.Working:
+                CancelTask();
+                ReturnToInitialPosition();
+                break;
+            case PlayerState.Returning:
+                break;
+            case PlayerState.Unconscious:
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void EnableMovement()
+    {
+        this.GetComponent<PlayerMovement>().enabled = true;
+    }
+    private void DisableMovement()
+    {
+        this.GetComponent<PlayerMovement>().enabled = false;
+    }
+
+    private void CancelTask()
+    {
+        Debug.Log("Caneling task");
+    }
+
+    private void ReturnToInitialPosition()
+    {
+        CurrentState = PlayerState.Returning;
+    }
+}

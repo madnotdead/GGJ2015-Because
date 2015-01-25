e5ad4a9841107a77cfadb3104af82543cb0074d0
Ezequiel Calderara <ezechico@gmail.com>
2015-01-25 15:31:07 -0300

    Subo cambios

--- Unity/Assets/Scripts/Objectives/PanelScript.cs
+++ Unity/Assets/Scripts/Objectives/PanelScript.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class PanelScript : MonoBehaviour {
 
-
-    public float tiltingTime = 3;
+    public Animator animator;
+    public bool IsRight;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        animator.SetBool("IsRight", IsRight);
+
+        animator.SetBool("Idling", false);
 	}
 }
